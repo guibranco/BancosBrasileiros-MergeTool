567f7b183cae22f86f961fdfb3e35cbfae8f0285
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-10-09 00:31:34 +0100

    Add source system to changelog (#28)
    Co-authored-by: Guilherme Branco Stracini <guilhermes@outsurance.ie>

--- BancosBrasileiros.MergeTool/Helpers/Source.cs
+++ BancosBrasileiros.MergeTool/Helpers/Source.cs
@@ -14,6 +14,8 @@
 
 namespace BancosBrasileiros.MergeTool.Helpers;
 
+using CrispyWaffle.Attributes;
+
 /// <summary>
 /// Enum Source.
 /// </summary>
@@ -22,60 +24,72 @@ public enum Source
     /// <summary>
     /// The base.
     /// </summary>
+    [HumanReadable("Base")]
     Base,
 
     /// <summary>
     /// The change log.
     /// </summary>
+    [HumanReadable("ChangeLog")]
     ChangeLog,
 
     /// <summary>
     /// The CQL.
     /// </summary>
+    [HumanReadable("CQL")]
     Cql,
 
     /// <summary>
     /// The CTC.
     /// </summary>
+    [HumanReadable("CTC")]
     Ctc,
 
     /// <summary>
     /// The detecta flow.
     /// </summary>
+    [HumanReadable("DetectaFlow")]
     DetectaFlow,
 
     /// <summary>
     /// The document.
     /// </summary>
+    [HumanReadable("Document")]
     Document,
 
     /// <summary>
     /// The PCPS.
     /// </summary>
+    [HumanReadable("PCPS")]
     Pcps,
 
     /// <summary>
     /// The SILOC.
     /// </summary>
+    [HumanReadable("SILOC")]
     Siloc,
 
     /// <summary>
     /// The SITRAF.
     /// </summary>
+    [HumanReadable("SITRAF")]
     Sitraf,
 
     /// <summary>
     /// The SLC.
     /// </summary>
+    [HumanReadable("SLC")]
     Slc,
 
     /// <summary>
     /// The SPI..
     /// </summary>
+    [HumanReadable("SPI")]
     Spi,
 
     /// <summary>
     /// The STR.
     /// </summary>
+    [HumanReadable("STR")]
     Str,
 }
