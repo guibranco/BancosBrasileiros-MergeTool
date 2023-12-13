67991eb82493a47c65fa8621979b545c3a21b425
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-12-13 12:22:12 +0000

    Update Seeder.cs (#58)
    * Update Seeder.cs
    
    * CSharpier format
    
    ---------
    
    Co-authored-by: gstraccini[bot] <44061807+gstraccini[bot]@users.noreply.github.com>

--- BancosBrasileiros.MergeTool/Helpers/Seeder.cs
+++ BancosBrasileiros.MergeTool/Helpers/Seeder.cs
@@ -395,7 +395,10 @@ internal class Seeder
                 && bank.DatePixStarted.Equals(spi.DatePixStarted)
             )
             {
-                Logger.Log($"SPI | PSP is updated: {spi.LongName}", ConsoleColor.DarkGreen);
+                Logger.Log(
+                    $"SPI | Bank {bank.Compe} is updated: {spi.LongName}",
+                    ConsoleColor.DarkGreen
+                );
 
                 upToDate++;
                 continue;
@@ -487,7 +490,7 @@ internal class Seeder
             if (bank == null)
             {
                 Logger.Log(
-                    $"CTC | Bank {ctc.Compe} not found: {ctc.LongName} | {ctc.Document.Trim()}",
+                    $"CTC | Bank {bank.Compe} not found: {ctc.LongName} | {ctc.Document.Trim()}",
                     ConsoleColor.DarkRed
                 );
                 notFound++;
@@ -601,7 +604,7 @@ internal class Seeder
             )
             {
                 Logger.Log(
-                    $"SILOC | Bank {siloc.Compe} COB/DOC is updated: {siloc.LongName}",
+                    $"SILOC | Bank {bank.Compe} COB/DOC is updated: {siloc.LongName}",
                     ConsoleColor.DarkGreen
                 );
                 upToDate++;
