0eb6695ec6e2dd853b4512a5d5c54c0c0ae1af53
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-06-19 15:00:04 +0100

    Update Constants.cs

--- BancosBrasileiros.MergeTool/Helpers/Constants.cs
+++ BancosBrasileiros.MergeTool/Helpers/Constants.cs
@@ -21,72 +21,72 @@ namespace BancosBrasileiros.MergeTool.Helpers;
 internal static class Constants
 {
     /// <summary>
-    /// The change log URL
+    /// The change log URL.
     /// </summary>
     public const string ChangeLogUrl =
         "https://raw.githubusercontent.com/guibranco/BancosBrasileiros/main/CHANGELOG.md";
 
     /// <summary>
-    /// The base URL
+    /// The base URL.
     /// </summary>
     public const string BaseUrl =
         "https://raw.githubusercontent.com/guibranco/BancosBrasileiros/main/data/bancos.json";
 
     /// <summary>
-    /// The string URL
+    /// The STR URL.
     /// </summary>
     public const string StrUrl =
         "https://www.bcb.gov.br/content/estabilidadefinanceira/str1/ParticipantesSTR.csv";
 
     /// <summary>
-    /// The spi URL
+    /// The SPI/PIX URL.
     /// </summary>
     public const string SpiUrl =
         "https://www.bcb.gov.br/content/estabilidadefinanceira/spi/participantes-spi-{0:yyyyMMdd}.csv";
 
     /// <summary>
-    /// The SLC URL
+    /// The SLC URL.
     /// </summary>
     public const string SlcUrl =
         "https://www2.nuclea.com.br/Monitoramento/Participantes_Homologados.pdf";
 
     /// <summary>
-    /// The siloc URL
+    /// The SILOC URL.
     /// </summary>
     public const string SilocUrl = "https://www2.nuclea.com.br/Monitoramento/SILOC.pdf";
 
     /// <summary>
-    /// The sitraf URL
+    /// The SITRAF URL.
     /// </summary>
     public const string SitrafUrl =
         "https://www2.nuclea.com.br/Monitoramento/Rela%C3%A7%C3%A3o%20de%20Clientes%20SITRAF.pdf";
 
     /// <summary>
-    /// The CTC URL
+    /// The CTC URL.
     /// </summary>
     public const string CtcUrl =
         "https://www2.nuclea.com.br/SAP/Rela%C3%A7%C3%A3o%20de%20Clientes%20CTC.pdf";
 
     /// <summary>
-    /// The PCPS URL
+    /// The PCPS URL.
     /// </summary>
     public const string PcpsUrl =
         "https://www2.nuclea.com.br/SAP/Rela%C3%A7%C3%A3o%20de%20Participantes%20PCPS.pdf";
 
     /// <summary>
-    /// The CQL URL
+    /// The CQL URL.
     /// </summary>
     public const string CqlUrl =
         "https://www2.nuclea.com.br/SAP/Rela%C3%A7%C3%A3o%20de%20Participantes%20CQL.pdf";
 
     /// <summary>
-    /// The DetectaFlow URL
+    /// The DetectaFlow URL.
     /// </summary>
     public const string DetectaFlowUrl =
         "https://www2.nuclea.com.br/SAP/Rela%C3%A7%C3%A3o%20de%20Participantes%20-%20Detecta%20Flow.pdf";
 
     /// <summary>
-    /// The PCR URL
+    /// The PCR URL.
     /// </summary>
     public const string PcrUrl =
         "https://www2.nuclea.com.br/SAP/Rela%C3%A7%C3%A3o%20de%20Clientes%20PCR.pdf";
